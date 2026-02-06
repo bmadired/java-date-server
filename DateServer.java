9099ee467743678452c0a0e74307d3c64bbffcd3
Seth7003 <sethwilkins3@gmail.com>
2026-02-06 10:56:59 -0600

    Update DateServer.java

--- DateServer.java
+++ DateServer.java
@@ -11,10 +11,40 @@ public class DateServer
             while (true) {
                 Socket client = sock.accept();
 
+                System.out.println("Client connected");
+
                 PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
-                
+                BufferedReader bin = new BufferedReader(
+                        new InputStreamReader(client.getInputStream()));
+                BufferedReader serverInput = new BufferedReader(
+                        new InputStreamReader(System.in));
+
                 /* write the Date to the socket */
                 pout.println(new java.util.Date().toString());
+
+                String clientMessage;
+                String serverMessage;
+
+                while (true) {
+
+                    clientMessage = bin.readLine();
+
+                    if (clientMessage == null ||
+                        clientMessage.equalsIgnoreCase("exit")) {
+                        break;
+                    }
+
+                    System.out.println("Client: " + clientMessage);
+
+                    serverMessage = serverInput.readLine();
+
+                    if (serverMessage.equalsIgnoreCase("exit")) {
+                        pout.println("exit");
+                        break;
+                    }
+
+                    pout.println(serverMessage);
+                }
 
                 /* close the socket and resume */
                 /* listening for connection */
