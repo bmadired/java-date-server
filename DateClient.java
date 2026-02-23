25f2a487d551003ada54b3c683d3781663436c0f
Seth7003 <sethwilkins3@gmail.com>
2026-02-23 13:23:09 -0600

    Update DateClient.java

--- DateClient.java
+++ DateClient.java
@@ -3,9 +3,11 @@ import java.io.*;
 
 public class DateClient
 {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         try {
             Socket sock = new Socket("172.16.41.197", 6013);
+
+            System.out.println("Connected to server");
 
             PrintWriter pout = new PrintWriter(sock.getOutputStream(), true);
             BufferedReader bin = new BufferedReader(
@@ -13,6 +15,7 @@ public class DateClient
             BufferedReader userInput = new BufferedReader(
                     new InputStreamReader(System.in));
 
+            /* read the Date from the server */
             System.out.println("Server: " + bin.readLine());
 
             String userMessage;
@@ -40,8 +43,8 @@ public class DateClient
                 System.out.println("Server: " + serverMessage);
             }
 
+            /* close the socket */
             sock.close();
-
         }
         catch (IOException ioe) {
             System.err.println(ioe);
