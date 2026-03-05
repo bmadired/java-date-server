ee7224442b41e817c8104f2d130ee5e2def04e16
Seth7003 <sethwilkins3@gmail.com>
2026-03-05 16:24:16 -0600

    Update DateServer.java

--- DateServer.java
+++ DateServer.java
@@ -3,6 +3,7 @@ import java.io.*;
 
 public class DateServer {
     static ClientHandler[] clients = new ClientHandler[100];
+    static String[] clientNames = new String[100];
     static int clientCount = 0;
     static int idCounter = 1;
 
@@ -10,27 +11,36 @@ public class DateServer {
         try {
             ServerSocket sock = new ServerSocket(6013);
 
-            new Thread(() -> {
-                BufferedReader serverInput = new BufferedReader(new InputStreamReader(System.in));
-                String input;
-                try {
-                    while ((input = serverInput.readLine()) != null) {
-                        if (input.equalsIgnoreCase("all")) {
-                            for (int i = 0; i < clientCount; i++) {
-                                System.out.println(clients[i].id + ". " + clients[i].name);
-                            }
-                        } else {
-                            try {
-                                int clientId = Integer.parseInt(input);
-                                ClientHandler target = findClientById(clientId);
-                                if (target != null) {
-                                    String msg = serverInput.readLine();
-                                    target.sendMessage("Server: " + msg);
+            new Thread(new Runnable() {
+                public void run() {
+                    BufferedReader serverInput =
+                            new BufferedReader(new InputStreamReader(System.in));
+                    String input;
+
+                    try {
+                        while ((input = serverInput.readLine()) != null) {
+                            if (input.equalsIgnoreCase("all")) {
+                                for (int i = 0; i < clientCount; i++) {
+                                    System.out.println(clients[i].id + ". " + clients[i].name);
                                 }
-                            } catch (NumberFormatException e) {}
+                            } else {
+                                try {
+                                    int clientId = Integer.parseInt(input);
+                                    ClientHandler target = findClientById(clientId);
+                                    if (target != null) {
+                                        String msg = serverInput.readLine();
+                                        if (msg.equalsIgnoreCase("end")) {
+                                            target.sendMessage("Connection was ended.");
+                                            target.endConnection();
+                                        } else {
+                                            target.sendMessage("Server: " + msg);
+                                        }
+                                    }
+                                } catch (NumberFormatException e) {}
+                            }
                         }
-                    }
-                } catch (IOException e) {}
+                    } catch (IOException e) {}
+                }
             }).start();
 
             while (true) {
@@ -38,28 +48,21 @@ public class DateServer {
                 ClientHandler handler = new ClientHandler(client);
                 new Thread(handler).start();
             }
-
         } catch (IOException ioe) {
             System.err.println(ioe);
         }
     }
 
-    static synchronized ClientHandler findClientById(int id) {
-        for (int i = 0; i < clientCount; i++) {
-            if (clients[i].id == id) return clients[i];
-        }
-        return null;
-    }
-
     static synchronized boolean nameExists(String name) {
         for (int i = 0; i < clientCount; i++) {
-            if (clients[i].name.equalsIgnoreCase(name)) return true;
+            if (clientNames[i].equalsIgnoreCase(name)) return true;
         }
         return false;
     }
 
     static synchronized void addClient(ClientHandler handler) {
         clients[clientCount] = handler;
+        clientNames[clientCount] = handler.name;
         clientCount++;
     }
 
@@ -68,6 +71,7 @@ public class DateServer {
             if (clients[i] == handler) {
                 for (int j = i; j < clientCount - 1; j++) {
                     clients[j] = clients[j + 1];
+                    clientNames[j] = clientNames[j + 1];
                 }
                 clientCount--;
                 break;
@@ -75,12 +79,11 @@ public class DateServer {
         }
     }
 
-    static synchronized void broadcast(String msg, ClientHandler sender) {
+    static synchronized ClientHandler findClientById(int id) {
         for (int i = 0; i < clientCount; i++) {
-            if (clients[i] != sender) {
-                clients[i].sendMessage(msg);
-            }
+            if (clients[i].id == id) return clients[i];
         }
+        return null;
     }
 
     static class ClientHandler implements Runnable {
@@ -97,37 +100,46 @@ public class DateServer {
         public void run() {
             try {
                 pout = new PrintWriter(socket.getOutputStream(), true);
-                bin  = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                bin = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
                 pout.println("Enter your name:");
                 name = bin.readLine();
+
                 while (nameExists(name)) {
                     pout.println("Name already taken. Enter another name:");
                     name = bin.readLine();
                 }
 
-                synchronized (DateServer.class) {
-                    id = idCounter++;
-                    addClient(this);
-                }
+                id = idCounter++;
+                addClient(this);
 
+                System.out.println("Client " + id + " has connected.");
                 pout.println("Your ID is: " + id);
 
                 String message;
                 while ((message = bin.readLine()) != null) {
-                    if (message.equalsIgnoreCase("exit")) break;
-                    broadcast(name + ": " + message, this);
+                    if (message.equalsIgnoreCase("end")) {
+                        System.out.println("Client " + id + " connection was ended.");
+                        break;
+                    }
+                    System.out.println(name + ": " + message);
                 }
 
-            } catch (IOException e) {
-            } finally {
                 removeClient(this);
-                try { socket.close(); } catch (IOException e) {}
-            }
+                socket.close();
+
+            } catch (IOException e) {}
         }
 
         void sendMessage(String msg) {
             pout.println(msg);
+        }
+
+        void endConnection() {
+            try {
+                removeClient(this);
+                socket.close();
+            } catch (IOException e) {}
         }
     }
 }
