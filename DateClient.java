307575e86cbd6422b4a899de30cd2c9a9d83e39a
Seth7003 <sethwilkins3@gmail.com>
2026-03-05 15:59:06 -0600

    Update DateClient.java

--- DateClient.java
+++ DateClient.java
@@ -4,17 +4,26 @@ import java.io.*;
 public class DateClient {
     public static void main(String[] args) {
         try {
-            Socket sock = new Socket("172.16.41.197", 6013);
+            Socket sock = new Socket("192.168.1.169", 6013);
 
             PrintWriter pout = new PrintWriter(sock.getOutputStream(), true);
             BufferedReader bin  = new BufferedReader(new InputStreamReader(sock.getInputStream()));
             BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
 
+            // Handle name registration before starting receiver thread
+            String line;
+            while ((line = bin.readLine()) != null) {
+                System.out.println(line);
+                if (line.startsWith("Your ID is:")) break;
+                pout.println(userInput.readLine());
+            }
+
+            // Only start receiver thread after registration is complete
             Thread receiver = new Thread(() -> {
                 try {
                     String incoming;
                     while ((incoming = bin.readLine()) != null) {
-                        System.out.println("Server: " + incoming);
+                        System.out.println(incoming);
                     }
                 } catch (IOException e) {}
             });
@@ -34,4 +43,3 @@ public class DateClient {
         }
     }
 }
-
