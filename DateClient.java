30a1c753a25ed0f41d385a7200e9bc501bd91b97
Seth7003 <sethwilkins3@gmail.com>
2026-03-11 22:54:03 -0500

    Update DateClient.java

--- DateClient.java
+++ DateClient.java
@@ -1,14 +1,13 @@
 import java.net.*;
 import java.io.*;
-
+ 
 public class DateClient {
     public static void main(String[] args) {
         try {
-            Socket sock = new Socket("172.16.41.197", 6013);
+            Socket sock = new Socket("192.168.1.169", 6013);
             PrintWriter pout = new PrintWriter(sock.getOutputStream(), true);
             BufferedReader bin = new BufferedReader(new InputStreamReader(sock.getInputStream()));
             BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
-
             new Thread(new Runnable() {
                 public void run() {
                     try {
@@ -22,16 +21,20 @@ public class DateClient {
                     } catch (IOException e) {}
                 }
             }).start();
-
             String userMessage;
             while ((userMessage = userInput.readLine()) != null) {
-                if (userMessage.equalsIgnoreCase("end")) {
-                    pout.println("end");
+                if (userMessage.equalsIgnoreCase("exit")) {
+                    pout.println("exit");
                     System.out.println("Connection was ended.");
                     break;
+                } else if (userMessage.equalsIgnoreCase("list")) {
+                    pout.println("list");
+                } else if (userMessage.equalsIgnoreCase("all")) {
+                    String broadMsg = userInput.readLine();
+                    pout.println("all");
+                    pout.println(broadMsg);
                 } else if (userMessage.matches("\\d+")) {
                     int targetId = Integer.parseInt(userMessage);
-                    System.out.println("Enter message for client " + targetId + ":");
                     String privateMsg = userInput.readLine();
                     pout.println("@" + targetId);
                     pout.println(privateMsg);
@@ -39,9 +42,7 @@ public class DateClient {
                     pout.println(userMessage);
                 }
             }
-
             sock.close();
-
         } catch (IOException ioe) {
             System.err.println(ioe);
         }
